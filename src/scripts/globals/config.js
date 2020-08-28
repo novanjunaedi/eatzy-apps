1e9b6be9614229a375be29a204b9396f0844d28b
Novan Junaedi <junaedinovan98@gmail.com>
2020-08-28 17:18:04 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  KEY: '12345',
+  BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
+  BASE_IMAGE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'eatzy-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
+};
+
+export default CONFIG;
